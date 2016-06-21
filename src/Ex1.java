965fbfe29dafdd39918cd8d3673a17b3a7feab27
Thu Thủy Nguyễn <Thu Thy Nguyn>
2016-06-22 01:02:46 +0700

    first commit

---
+++ src/Ex1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Thu Thuy Nguyen on 6/20/2016.
+ */
+public class Ex1 {
+    public static void main (String [] args) {
+        float a = (float) (((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+        System.out.println(a);
+    }
+}
---
+++ src/Ex2.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Thu Thuy Nguyen on 6/20/2016.
+ */
+public class Ex2 {
+
+}
